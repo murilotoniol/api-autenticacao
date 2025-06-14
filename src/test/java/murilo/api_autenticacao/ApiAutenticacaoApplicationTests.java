0728851662888da21dd29a8ddd7e094681a6143e
Murilo Besson <murilo.besson@outlook.com>
2025-06-14 20:51:09 -0300

    Criando repositorio trabalho

---
+++ src/test/java/murilo/api_autenticacao/ApiAutenticacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package murilo.api_autenticacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAutenticacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
