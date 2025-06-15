29d5432b63a0e09c2d013809a23c6b2c07b32766
Murilo Besson <murilo.besson@outlook.com>
2025-06-14 21:49:01 -0300

    Adicionando entidades Role, User e UserRepository

---
+++ src/main/java/murilo/api_autenticacao/models/Role.java
@@ -0,0 +1,6 @@
+package murilo.api_autenticacao.models;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ src/main/java/murilo/api_autenticacao/repositories/UserRepository.java
@@ -0,0 +1,16 @@
+package murilo.api_autenticacao.repositories;
+
+
+import murilo.api_autenticacao.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.swing.*;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(Spring email);
+
+}
