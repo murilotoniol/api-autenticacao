06fe382e39312a040144e21606e27fa7d8bec071
Murilo Besson <murilo.besson@outlook.com>
2025-06-15 17:24:03 -0300

    Criando Controladora de autenticação AuthController

---
+++ src/main/java/murilo/api_autenticacao/controllers/AuthController.java
@@ -0,0 +1,39 @@
+package murilo.api_autenticacao.controllers;
+
+import lombok.RequiredArgsConstructor;
+import murilo.api_autenticacao.dtos.AuthRequestDTO;
+import murilo.api_autenticacao.dtos.AuthResponseDTO;
+import murilo.api_autenticacao.models.User;
+import murilo.api_autenticacao.repositories.UserRepository;
+import murilo.api_autenticacao.services.JwtService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.PasswordAuthentication;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponseDTO> login(@RequestBody AuthRequestDTO request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword())
+        );
+        User user = userRepository.findByEmail(request.getEmail()).orElseThrow();
+        String token = jwtService.generateToken(user);
+        return ResponseEntity.ok(new AuthResponseDTO(token));
+    }
+}
